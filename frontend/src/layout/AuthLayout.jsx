c7c4a599efa5a8239546f9657009a33b6072e179
Neaz Morshed Sajid <160872389+morshedsajid26@users.noreply.github.com>
2026-08-27 20:03:16 +0600

    auth layput padding fixed

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -6,7 +6,7 @@ import Image from "../components/Image";
 export default function AuthLayout() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-t from-[#000003] to-[#060F3F] p-4">
-      <div className="w-[580px] bg-linear-to-b from-[#000006] to-[#050C35] rounded-[32px] px-20 py-12 flex flex-col items-center border border-[#1E293B]/40 shadow-2xl font-montserrat">
+      <div className="w-full max-w-[580px] bg-linear-to-b from-[#000006] to-[#050C35] rounded-[32px] px-6 sm:px-12 md:px-20 py-8 md:py-12 flex flex-col items-center border border-[#1E293B]/40 shadow-2xl font-montserrat">
       
       {/* Logo */}
       <Link to="/" className="flex items-center gap-2 mb-6 cursor-pointer">
